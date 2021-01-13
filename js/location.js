ed083045249da11af7d915a0ce8d6f3c98e306c5
Lloyd Chambrier <lloydchambrier@gmail.com>
2021-01-13 17:49:22 +0100

    Fixed map issue

--- js/location.js
+++ js/location.js
@@ -1,12 +1,10 @@
-const mymap = L.map('mapid').setView([52.36, 4.90], 12);
- 
-L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}', {
-    attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
-    maxZoom: 18,
-    id: 'mapbox.streets',
-    accessToken: 'pk.eyJ1IjoibGxveWQtY2hhbWJyaWVyIiwiYSI6ImNqcmh4NHd0ajJyZ3I0M24wZjJnOWVwZzgifQ.SaVzj9bmSvp4Zg7FhwC_Mg'
-    // make token url restrictions after deployment
-    }).addTo(mymap);
+const mymap = L.map('mapid', {center: [52.36, 4.90], zoom: 12 });
 
-const marker = L.marker([52.39, 4.90]).addTo(mymap);
-marker.bindPopup("<b><a href = 'https://treehousetribe.nl' target='_new'>Treehouse Tribe</a></b><br><p>You can find me here on business days :) </p>").openPopup();
+L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+    attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+    maxZoom: 18,
+    id: 'mapbox/streets-v11',
+    tileSize: 512,
+    zoomOffset: -1,
+    accessToken: 'pk.eyJ1IjoibGxveWQtY2hhbWJyaWVyIiwiYSI6ImNranZucDNocjBhYmoyb2w3azRhM2doZjMifQ.hou2WuO-vj5QnOGcx6Qd1Q'
+}).addTo(mymap);
